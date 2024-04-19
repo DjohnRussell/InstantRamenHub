05bb4a35d0f2188fab395b8774210af124fe6c69
danieljohnrussell <danieljr@hiof.no>
2024-04-19 20:32:59 +0200

    startet the thing :)

---
+++ instantRamenHub/src/main/java/com/example/instantRamenHub/InstantRamenHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.instantRamenHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantRamenHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantRamenHubApplication.class, args);
+	}
+
+}
---
+++ instantRamenHub/src/test/java/com/example/instantRamenHub/InstantRamenHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instantRamenHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantRamenHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
